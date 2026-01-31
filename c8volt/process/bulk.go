d8ecb5e175ffdc82be8e195d9fe0085044aca78a
Adam Boczek <adam.boczek@nttdata.com>
2026-01-31 07:38:50 +0100

    fix(typo): update various log messages

--- c8volt/process/bulk.go
+++ c8volt/process/bulk.go
@@ -47,7 +47,7 @@ func (c *client) CancelProcessInstances(ctx context.Context, keys types.Keys, wa
 	}
 	if !cCfg.NoWait {
 		t, oks, noks := r.Totals()
-		c.log.Info(fmt.Sprintf("cancelling %d process instance(s) completed: %d succeeded or already cancelled/teminated, %d failed", t, oks, noks))
+		c.log.Info(fmt.Sprintf("cancelling %d process instance(s) completed: %d succeeded or already cancelled/terminated, %d failed", t, oks, noks))
 	}
 	return r, err
 }
